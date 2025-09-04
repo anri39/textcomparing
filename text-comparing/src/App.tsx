e7cd4129a9b47459e4477341eaec274c62fd0a4d
Anri Ugrekhelidze <anriugrexelidze2006@gmail.com>
2025-09-04 15:10:16 +0400

    Sidebar design finished

--- text-comparing/src/App.tsx
+++ text-comparing/src/App.tsx
@@ -3,10 +3,12 @@ import FirstPartDropdown from "./components/FirstPartDropdown";
 import LanguageSelection from "./components/LanguageSelection";
 import NavBar from "./components/NavBar";
 import InputBoxes from "./components/InputBoxes";
+import SideMenu from "./components/SideMenu";
 
 function App() {
   return (
     <>
+      <SideMenu />
       <NavBar />
       <FirstPartDropdown />
       <LanguageSelection />
---
+++ text-comparing/src/components/SideMenu.tsx
@@ -0,0 +1,48 @@
+import "./SideMenu.css";
+import { Mic, AudioLines, FileText, Check, MoreHorizontal } from "lucide-react";
+import mainlogo from "../assets/mainlogo.svg";
+import Aa from "../assets/Group.svg";
+import ProfilePic from "../assets/profile.svg";
+
+const menuItems = [
+  { id: 1, label: "მართლმწერი", icon: Check },
+  { id: 2, label: "ტექსტის შედარება", icon: Aa, isImage: true },
+  { id: 3, label: "ხმა → ტესტი", icon: Mic },
+  { id: 4, label: "ტექსტი → ხმა", icon: AudioLines },
+  { id: 5, label: "PDF კონვერტაცია", icon: FileText },
+];
+
+export default function SideMenu() {
+  return (
+    <aside
+      className="sidemenu"
+      style={{
+        background: "#132450",
+      }}
+    >
+      <div className="sidemenu__logo">
+        <img src={mainlogo} alt="Logo" />
+        <h1>ENAGRAM</h1>
+      </div>
+
+      <nav className="sidemenu__menu">
+        {menuItems.map((item) => (
+          <div key={item.id} className="sidemenu__menu-item">
+            {item.isImage ? (
+              <img src={item.icon} alt={item.label} />
+            ) : (
+              <item.icon size={24} />
+            )}
+            <p>{item.label}</p>
+          </div>
+        ))}
+      </nav>
+
+      <div className="sidemenu__bottom">
+        <img src={ProfilePic} alt="User" />
+        <p className="fullname">თამარ ონიანი</p>
+        <MoreHorizontal size={20} />
+      </div>
+    </aside>
+  );
+}
